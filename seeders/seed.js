9ae842e024deb7e439372a2b2b5ad9b0ba4afba1
xuannga <nguyenx206@gmail.com>
2021-09-15 21:33:02 -0700

    working on deployment

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-// mongoose.connect('mongodb://localhost/workout', {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-// });
-
-console.log(process.env.MONGODB_URI);
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect('mongodb://localhost/workout', {
     useNewUrlParser: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
 });
+
+//console.log(process.env.MONGODB_URI);
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+// });
 
 
 
