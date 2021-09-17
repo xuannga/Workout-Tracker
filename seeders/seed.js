0401072f245a20f32179df845f496b419e635426
xuannga <nguyenx206@gmail.com>
2021-09-17 15:34:56 -0700

    updates

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,21 +1,6 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-// mongoose.connect('mongodb://localhost/workout', {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-// });
-
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }
-);
-
 
 
 const workoutSeed = [{
