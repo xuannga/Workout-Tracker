09c2b251dd88968d52d67930092a9ee9b3386360
xuannga <nguyenx206@gmail.com>
2021-09-18 17:08:37 -0700

    updated

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/html-routes"));
